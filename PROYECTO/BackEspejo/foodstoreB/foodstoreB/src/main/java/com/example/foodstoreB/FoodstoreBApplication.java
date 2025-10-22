d67485126ca0c18a56132c238be05792474b5321
MentalHazee <santosjulian1997@gmail.com>
2025-10-21 23:07:42 -0300

    creado carpetas del back

---
+++ PROYECTO/BackEspejo/foodstoreB/foodstoreB/src/main/java/com/example/foodstoreB/FoodstoreBApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodstoreB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodstoreBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodstoreBApplication.class, args);
+	}
+
+}
---
+++ PROYECTO/BackEspejo/foodstoreB/foodstoreB/src/test/java/com/example/foodstoreB/FoodstoreBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodstoreB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodstoreBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
